7df7b561a1d1bc1e64cce57e3e378ffdea4e588a
Muhammad Gilang Ramadhan <116289802+23okt@users.noreply.github.com>
2024-10-02 15:58:23 +0700

    Delete app directory

--- app/script.js
+++
@@ -1,9 +0,0 @@
-const starContainer = document.querySelectorAll(".stars-container");
-for (let container of starContainer) {
-  for (let i = 0; i < 5; i++) {
-    const star = document.createElement("img");
-    star.src = "../assets/star.png";
-    star.classList.add("w-5", "h-5");
-    container.appendChild(star);
-  }
-}
